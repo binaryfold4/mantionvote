e7bbca5a696b5cd11488ddaa7311213ffe178b57
Ryan Verner <ryan.verner@squareweave.com.au>
2015-05-23 22:42:35 +1000

    Further config/deployment changes

--- mantionvote/settings_local_example.py
+++ mantionvote/settings_local_example.py
@@ -1,5 +1,23 @@
-# Rename to settings_local.py
-SECRET_KEY = 'dfgdfg'
-SOCIAL_AUTH_SOUNDCLOUD_KEY = 'dfgdfg'
-SOCIAL_AUTH_SOUNDCLOUD_SECRET = 'dfgdf'
-DEBUG = True
+# Rename to settings_local.py and customise
+
+SECRET_KEY = 'MustBeUniquePleaseChange'
+SOCIAL_AUTH_SOUNDCLOUD_KEY = ''
+SOCIAL_AUTH_SOUNDCLOUD_SECRET = ''
+
+DEBUG = False
+ALLOWED_HOSTS = [ 'domain.name.here' ]
+
+# comment this out to use default sqlite database (not in production)
+DATABASES = {
+    'default': {
+        'ENGINE':   'django.db.backends.mysql',
+        'NAME':     '',
+        'USER':     '',
+        'PASSWORD': '',
+    }
+}
+
+ADMINS = (
+	 ('Ryan Verner', 'ryan.verner@gmail.com'),
+)
+
